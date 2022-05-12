1944ea049bfde63a7e71469d2f8f32914cc64e96
Zaba505 <cakub6@gmx.com>
2022-05-11 23:44:23 -0400

    feat(cmd/anirent): execute root cmd with context for easy signal handling (#10)

--- cmd/anirent/cmd/cmd.go
+++ cmd/anirent/cmd/cmd.go
@@ -1,13 +1,18 @@
 package cmd
 
 import (
+	"context"
 	"os"
+	"os/signal"
 )
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := rootCmd.ExecuteContext(ctx)
 	if err != nil {
 		os.Exit(1)
 	}
